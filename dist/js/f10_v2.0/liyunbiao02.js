3d9c9a1b27ebc24db20da1aef4bb11cf56b0b9de
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-06-29 12:51:01 +0800

    快速提交

---
+++ dist/js/f10_v2.0/liyunbiao02.js
@@ -0,0 +1,21 @@
+
+$(function(){
+    var tableScroll = new IScroll('#rightBox', {
+        scrollX:true,
+        scrollY:false,
+        bounce:false,
+        startX:-100,
+        //preventDefault:true,
+        eventPassthrough:true
+    });
+    tableScroll.scrollTo(0,0,1000,IScroll.utils.ease.bounce);
+
+    /*$('#fixBottom').on('click','li',function(){
+        var curIndex=$(this).index();
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+    });
+    alert('ceshi_p01'+$('#ceshi_p01').height());
+    alert('ceshi_p02'+$('#ceshi_p02').height());*/
+});
